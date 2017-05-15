754fd4cd6ba6a2ba640165378df1ac9fb262e94e
Brent Lunghino <brentlunghino@gmail.com>
2017-05-15 11:26:50 -0700

    added imageio backend and tifffile backend

--- read_in_data.py
+++ read_in_data.py
@@ -2,6 +2,7 @@ import os.path
 from PIL import Image
 
 import cv2
+import skimage.io
 import pandas as pd 
 from sklearn.preprocessing import MultiLabelBinarizer
 import torch
@@ -16,7 +17,7 @@ class AmazonDataset(Dataset):
     class to conform data to pytorch API
     """
     def __init__(self, csv_path, img_path, dtype,
-                 img_ext='.jpg', backend='freeimage'):
+                 img_ext='.jpg', backend=None):
     
         self.img_path = img_path
         self.img_ext = img_ext
@@ -37,14 +38,17 @@ class AmazonDataset(Dataset):
         return X_train image and y_train index
         """
         img_str = self.X_train[index] + self.img_ext
+        load_path = os.path.join(self.img_path, img_str)
         ## branching for different backends
-        if self.backend == 'freeimage':
-            img = []
-        elif self.backend == 'opencv':
-            img = cv2.imread(os.path.join(self.img_path, img_str), -1)
-        ## PIL default, this will work for .jpg but not 16-bit tiffs
+        ## 'freeimage' only does 3 bands
+        if self.backend == 'imageio':
+            img = skimage.io.imread(load_path, plugin='imageio')
+        ## PIL , this will work for .jpg but not 16-bit tiffs
+        elif self.backend == 'PIL':
+            img = Image.open(load_path)
+        ## tifffile
         else:
-            img = Image.open(os.path.join(self.img_path, img_str))
+            img = skimage.io.imread(load_path, plugin='tifffile')
         img = self.transforms(img)
         label = torch.from_numpy(self.y_train[index]).type(self.dtype)
         return img, label
@@ -56,8 +60,8 @@ class AmazonDataset(Dataset):
 if __name__ == '__main__':
     csv_path = 'data/train_v2.csv'
     img_path = 'data/train-jpg'
-    dtype = torch.FloatTensor
-    training_dataset = AmazonDataset(csv_path, img_path, dtype, backend='freeimage')
+    dtype = torch.FlogatTensor
+    training_dataset = AmazonDataset(csv_path, img_path, dtype)
     train_loader = DataLoader(
         training_dataset,
         batch_size=256,
