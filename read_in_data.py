c367837da7bfcc0cc49626511879b95f4650877c
Vishal Subbiah <subbiahvishal@gmail.com>
2017-06-23 13:15:37 -0700

    modifying for npy

--- read_in_data.py
+++ read_in_data.py
@@ -68,6 +68,7 @@ class ResnetTrainDataset(Dataset):
 
         elif self.img_ext == '.npy':
             img = np.load(load_path)
+            img=np.array(img,dtype=np.int32)
 
         label = torch.from_numpy(self.y_train[index]).type(self.dtype)
         return img, label
